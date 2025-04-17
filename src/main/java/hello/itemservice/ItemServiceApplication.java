18f06637e0eb26e43b33fb3d227ac10880a54f99
Theo <kkw.theo@gmail.com>
2025-04-17 11:50:53 +0900

    feat: JdbcTemplate - SimpleJdbcInsert

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
